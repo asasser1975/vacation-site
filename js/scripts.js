45f7ad085c6ab41d3d8604046818c044032de1c8
Alex Sasser <a.sasser1975@gmail.com>
2019-02-22 14:19:02 -0800

    add class to headers in questions. Start js building of question answer collection.

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,34 @@ var result = function(q1answer, q2answer, q3answer, q4answer, q5answer, q6answer
 };
 
 // Front End Logic
+
+
 $(document).ready(function() {
 
-  $("form#questions").submit(function() {
+
+// Pull question answers
+  $("form#q1").submit(function() {
     var q1answer = $("input:radio[name=choice1]:checked").val();
+    event.preventDefault();
+  });
+  $("form#q2").submit(function() {
     var q2answer = $("input:radio[name=choice2]:checked").val();
+    event.preventDefault();
+  });
+  $("form#q3").submit(function() {
     var q3answer = $("input:radio[name=choice3]:checked").val();
+    event.preventDefault();
+  });
+  $("form#q4").submit(function() {
     var q4answer = $("input:radio[name=choice4]:checked").val();
+    event.preventDefault();
+  });
+  $("form#q1").submit(function() {
     var q5answer = $("input:radio[name=choice5]:checked").val();
     event.preventDefault();
-  }
+  });
+
+
 
 
 }
