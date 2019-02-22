efb227e09f3f43946f16394f95c6483eb0bf0f31
Alex Sasser <a.sasser1975@gmail.com>
2019-02-22 13:12:01 -0800

    build out headers for questions; flesh out script for website.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
+// Backend (Business) Logic
+var result = q1 + q2 + q3 + q4 + q5 + q6;
+
+
+// Front End Logic
 $(document).ready(function() {
-  event.preventDefault();
 
-  // Backend (Business) Logic
-  var result = q1 + q2 + q3 + q4 + q5 + q6;
+  $("form#questions").submit(function() {
+    var q1answer = $("input:radio[name=choice1]:checked").val();
+    var q2answer = $("input:radio[name=choice2]:checked").val();
+    var q3answer = $("input:radio[name=choice3]:checked").val();
+    var q4answer = $("input:radio[name=choice4]:checked").val();
+    var q5answer = $("input:radio[name=choice5]:checked").val();
+    event.preventDefault();
+  }
 
-  // Front End Logic
-  
+
 }
