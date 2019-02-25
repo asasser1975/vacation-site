96e201a4c289129c6d59b08e14122d9e87520465
Alex Sasser <a.sasser1975@gmail.com>
2019-02-24 17:23:27 -0800

    create final project.

--- js/scripts.js
+++ js/scripts.js
@@ -26,8 +26,7 @@ $(document).ready(function() {
     $("#findout").removeClass("hide");
   });
 
-
-  $("form#question").submit(function() {
+  $("form#questions").submit(function() {
     var q1answer = parseInt($("input:radio[name=choice1]:checked").val());
     var q2answer = parseInt($("input:radio[name=choice2]:checked").val());
     var q3answer = parseInt($("input:radio[name=choice3]:checked").val());
@@ -37,16 +36,28 @@ $(document).ready(function() {
     var result = add(q1answer, q2answer, q3answer, q4answer, q5answer);
 
     if(result <= 8) {
+      $("#q5").slideToggle();
+      $("#findout").addClass("hide");
+      $("#q1").addClass("hide");
       $('#iceland').removeClass("hide");
     } else if(result >= 9 && result <= 13) {
+      $("#q5").slideToggle();
+      $("#findout").addClass("hide");
       $('#outback').removeClass("hide");
     } else if(result >= 14 && result <= 17) {
+      $("#q5").slideToggle();
+      $("#findout").addClass("hide");
       $('#sea').removeClass("hide");
     } else if(result >= 18 && result <= 21) {
+      $("#q5").slideToggle();
+      $("#findout").addClass("hide");
       $('#rio').removeClass("hide");
     } else {
+      $("#q5").slideToggle();
+      $("#findout").addClass("hide");
       $('#nyc').removeClass("hide");
     }
-        event.preventDefault();
+
+    event.preventDefault();
   });
 });
